d12a05405adbf94632c178247f7f10a7ea4d0702
Kartik Sharma <ksb40968@gmail.com>
2024-06-07 20:38:54 +0530

    First solution file added

--- Solutions/src/Day1/swapTwoNumbers.java
+++ Solutions/src/Day1/swapTwoNumbers.java
@@ -1,5 +1,20 @@
 package Day1;
 
-public class swapTwoNumbers {
+import java.util.Scanner;
+
+public class swapTwoNumbers{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter your second number: ");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping first number is: " + a +" & "+"second number is: "+b );
+    }
 
 }
