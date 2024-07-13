4d26b276f555fed020cec7f9de9db6f1afa5357c
Kartik Sharma <ksb40968@gmail.com>
2024-07-13 14:31:10 +0530

    2 Q done

--- Solutions/src/Demo.java
+++ Solutions/src/Demo.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 
 class Demo{
 
-    // swap two number
+    // reverseANumber
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        int num = sc.nextInt();
+        int rev = 0;
 
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
-
-        System.out.println(num1+","+num2);
+        while (num!=0){
+            rev = rev*10 + num%10;
+            num = num/10;
+        }
+        System.out.println(rev);
 
     }
 
