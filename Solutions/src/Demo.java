0582966c554652c619f2e45554b0717b9d01c9ed
Kartik Sharma <ksb40968@gmail.com>
2024-07-13 14:27:52 +0530

    Demo file updated

--- Solutions/src/Demo.java
+++ Solutions/src/Demo.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 
 class Demo{
 
-    // Swap two numbers
+    // swap two number
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-       int num1 = sc.nextInt();
-       int num2 = sc.nextInt();
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
-       int temp = num1;
-       num1 = num2;
-       num2 = temp;
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
 
-        System.out.println(num1 + "," + num2);
-
+        System.out.println(num1+","+num2);
 
     }
-}
+
+    }
