60263ebee5634a017f12491b5b3aafe8196422f4
Kartik Sharma <ksb40968@gmail.com>
2024-07-15 16:53:08 +0530

    new Q4 done

--- Solutions/src/Demo.java
+++ Solutions/src/Demo.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
-// reverse a number
+
+// reverse a String
 
 public class  Demo{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        System.out.println(myFunction(num1 , num2));
+   String name = sc.nextLine();
+
+   StringBuilder sb = new StringBuilder();
+   sb.append(name);
+        System.out.println(sb.reverse());
 
     }
 
-    static String myFunction(int num1, int num2){
 
-        int temp  =  num1;
-        num1 = num2;
-        num2 = temp;
-
-        return num1 + "," + num2 ;
-
-    }
 }
 
 
