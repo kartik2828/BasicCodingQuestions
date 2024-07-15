d6883e15ff539910b10e9eaab562380aff2ca3ad
Kartik Sharma <ksb40968@gmail.com>
2024-07-15 17:21:56 +0530

    new Q6 done

--- Solutions/src/Demo.java
+++ Solutions/src/Demo.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
 
-// reverse a number
+// count of digits of an array
 
 public class  Demo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int rev = 0;
-        while (num != 0) {
 
-            rev = rev * 10 + num % 10;
-            num = num / 10;
+        // taking array size
+        System.out.println("Enter the array size: ");
+        int size = sc.nextInt();
 
+        int[] arr = new int[size];
+
+        // taking inputs for an array
+        System.out.println("Write the input: ");
+        for (int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
         }
-        System.out.println(rev);
 
+        // output
+        System.out.println(myfunction(arr));
 
+    }
+
+    static int myfunction(int arr[]){
+        int count = 0;
+        for (int i=0; i< arr.length; i++){
+            count ++;
+        }
+        return count;
     }
 }
 
