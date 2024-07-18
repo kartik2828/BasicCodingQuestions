fc0fd72a7112ac491427442acd57839da8a7fd1b
Kartik Sharma <ksb40968@gmail.com>
2024-07-18 14:33:56 +0530

    new 5 done

--- Solutions/src/Demo.java
+++ Solutions/src/Demo.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// prime number
+// Smallest and largest number
 
 public class  Demo {
 
@@ -12,20 +12,30 @@ public class  Demo {
         System.out.println("Enter the number size: ");
         int size = sc.nextInt();
 
+        int arr[] =  new int[size];
         // output
-       boolean result  = myFunction(size);
-        System.out.println(result);
-
+        System.out.println("Enter array element");
+        for (int i =0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+     myFunction(arr);
 
     }
-    static boolean myFunction(int num){
-        for (int i=2; i<=Math.sqrt(num); i++){
-            if (num%i==0){
-                return false;
+    static void myFunction(int[] num){
+        int min =  Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i=0; i<num.length; i++){
+            if (num[i]>max){
+                max = num[i];
+            }
+            if (num[i]<min){
+                min = num[i];
             }
 
         }
-        return true;
+        System.out.println("Maximum value"+ max);
+        System.out.println("Minimum value"+ min);
     }
 
 }
